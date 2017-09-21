64e1d9eecddd3c48f0d6a5d300f774130fd7bf74
Pierre Colle <piercus@gmail.com>
2017-09-21 09:36:28 +0200

    better version

--- public/index.js
+++ public/index.js
@@ -1,9 +1,49 @@
 
-
+(function ($) {
+    $.fn.replaceClass = function (pFromClass, pToClass) {
+        return this.removeClass(pFromClass).addClass(pToClass);
+    };
+}(jQuery));
 
 var send = function(message, options){
-  socket.emit(message, options)
+  console.log("send", message, options)
+  console.log("Socket io is not ready")
 };
+
+var jingleTimeout, jingleEnd, audioElem;
+
+var launchJingle = function(){
+  $(".opened").replaceClass("opened", "closed");
+  $(".selected").replaceClass("selected", "unselected");
+  $(".logo").replaceClass("unselected", "selected");
+  if(serverData.page === "front"){
+    audioElem = new Audio();
+    audioElem.src = "jingle.mp3";
+    audioElem.play();
+    jingleEnd = function(){
+      $(".logo").replaceClass("selected", "unselected");
+      open({ questionIndex : 0});
+    };
+    jingleTimeout = setTimeout(jingleEnd,60000)
+  }
+};
+
+var stopJingle = function(){
+  audioElem && audioElem.pause();
+  audioElem = null;
+
+  jingleEnd && jingleEnd();
+  jingleEnd = null;
+
+  jingleTimeout && clearTimeout(jingleTimeout);
+  jingleTimeout = null;
+};
+
+var open = function(options){
+  $(".opened").replaceClass("opened", "closed");
+  $(".selected").replaceClass("selected", "unselected");
+  $("#question-"+options.questionIndex).replaceClass("unselected", "selected");
+}
 
 var fns = {
 
@@ -18,44 +58,75 @@ var fns = {
   },
 
   go : function(options){
-    $(".opened").replaceClass("opened", "closed");
-    $(".selected").replaceClass("selected", "unselected");
-    $("#question-"+options.questionIndex).replaceClass("unselected", "selected");
+    stopJingle();
+    open(options)
   },
 
   answer : function(options){
-    $("#question-"+options.questionIndex+"-answer-"+options.answerIndex).replaceClass("closed", "opened");
-    var audioElem = new Audio();
-    audioElem.src = "./answer.wav";
-    audioElem.play();
+    stopJingle();
+    if(options.cancel){
+      $("#question-"+options.questionIndex+"-answer-"+options.answerIndex).replaceClass("opened", "closed");
+      side = $("#question-"+options.questionIndex+"-answer-"+options.answerIndex+" .cancel-control button")[0].attributes["side"].value
+      if(side && side !== ""){
+        var teamPoints = parseInt($(".team-"+side+" .team-points")[0].innerText)
+        teamPoints-=options.value;
+        $(".team-"+side+" .team-points")[0].innerText = teamPoints;
+        $("#question-"+options.questionIndex+"-answer-"+options.answerIndex+" .cancel-control button")[0].attributes["side"].value = ""
+      }
+      var audioElem = new Audio();
+      audioElem.src = "./cancel.wav";
+      audioElem.play();
+    } else {
+      $("#question-"+options.questionIndex+"-answer-"+options.answerIndex).replaceClass("closed", "opened");
+      var side = options.side;
+      var teamPoints = parseInt($(".team-"+side+" .team-points")[0].innerText)
+      teamPoints+=options.value;
+      $("#question-"+options.questionIndex+"-answer-"+options.answerIndex+" .cancel-control button")[0].attributes["side"].value = side;
+      $(".team-"+side+" .team-points")[0].innerText = teamPoints;
+      var audioElem = new Audio();
+      audioElem.src = "./answer.wav";
+      audioElem.play();
+    }
+  },
+
+  media : function(options){
+      $("#question-"+options.questionIndex).replaceClass("non-media", "non-answers");
+  },
+  unmedia : function(options){
+    $("#question-"+options.questionIndex).replaceClass("non-answers", "non-media");
   },
 
   reset : function(){
-    $(".opened").replaceClass("opened", "closed");
-    $(".selected").replaceClass("selected", "unselected");
-    $(".logo").replaceClass("hidden", "display");
-    var audioElem = new Audio();
-    audioElem.src = "./jingle.wav";
-    audioElem.play();
-    setTimeout(function(){
-      $(".logo").replaceClass("displayed", "hidden");
-      fns.go({ questionIndex : 0});
-    },4000)
+    stopJingle();
+    launchJingle();
+  },
+
+  displayAll : function(options){
+    $(".answer-of-question-"+options.questionIndex).replaceClass("closed", "opened");
   }
 
 };
 
 var onMessage = function(message, options){
+  console.log("receive", message, options);
   fns[message](options);
 };
 
 var onLoading = function(){
-  socket.connect("");
-  fns.reset();
+  if(typeof(serverData.currentQuestion) !== "number"){
+    fns.reset();
+  }
   var socket = io();
-  var events = ["wrong", "go", "answer", "reset"];
+  var events = ["wrong", "go", "answer", "reset", "displayAll", "media", "unmedia"];
 
   for(var i = 0; i < events.length; i++){
     socket.on(events[i], onMessage.bind(this, events[i]));
   }
+  send = function(message, options){
+    console.log("send", message, options)
+    socket.emit(message, options);
+    fns[message](options);
+  };
 };
+
+onLoading()
--- server.js
+++ server.js
@@ -8,15 +8,44 @@ var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 var port = process.env.PORT || 3000;
 
+var data = JSON.parse(fs.readFileSync("data.json"));
+data.currentQuestion = null;
+
 server.listen(port, function () {
   console.log('Server listening at port %d', port);
 });
 
-var events = ["wrong", "go", "answer", "reset"];
+var events = ["wrong", "go", "answer", "reset", "displayAll", "media", "unmedia"];
 
-var onEvent = function (socket, event, data) {
+var onEvent = function (socket, event, opts) {
+  if(event === "go"){
+    data.currentQuestion = opts.questionIndex;
+  } else if(event === "reset"){
+    data.currentQuestion = null;
+    for(var i = 0; i < data.questions.length; i++){
+      for(var j = 0; j < data.questions[i].answers.length; j++){
+        data.questions[i].answers[j].open = false;
+        data.questions[i].answers[j].side = null;
+      }
+    }
+  } else if(event === "answer"){
+    if(opts.cancel){
+      data.questions[opts.questionIndex].answers[opts.answerIndex].open = false;
+      data.questions[opts.questionIndex].answers[opts.answerIndex].side = null;
+    } else {
+      data.questions[opts.questionIndex].answers[opts.answerIndex].open = true;
+      data.questions[opts.questionIndex].answers[opts.answerIndex].side = opts.side;
+    }
+  } else if(event === "displayAll"){
+    for(var j = 0; j < data.questions[opts.questionIndex].answers.length; j++){
+      if(!data.questions[opts.questionIndex].answers[j].open){
+        data.questions[opts.questionIndex].answers[j].open = true;
+        data.questions[opts.questionIndex].answers[j].side = null;
+      }
+    }
+  }
   // we tell the client to execute 'new message'
-  socket.broadcast.emit(event, data);
+  socket.broadcast.emit(event, opts);
 };
 
 io.on('connection', function (socket) {
@@ -26,7 +55,7 @@ io.on('connection', function (socket) {
   // when the user disconnects.. perform this
   socket.on('disconnect', function (reason) {
     // debug
-    console.log(socket);
+    // console.log(socket);
     // log
     console.log("client "+socket.id+" disconnect because of ", reason)
   });
@@ -35,9 +64,10 @@ io.on('connection', function (socket) {
 app.set('view engine', 'ejs');
 app.use(express.static('public'))
 
-var data = JSON.parse(fs.readFileSync("data.json"));
+
 
 app.get('/front', function (req, res) {
+  //console.log("data.currentQuestion", data.currentQuestion);
   res.render('page', Object.assign({}, data, {"page" : "front"}));
 });
 
